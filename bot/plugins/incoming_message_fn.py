36e4279576a1624ae8c34403d7252a12cdb8460d
Abir Hasan <hackitabirhasan@gmail.com>
2021-03-02 19:54:33 +0600

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -20,7 +20,7 @@ from bot import (
   DOWNLOAD_LOCATION, 
   AUTH_USERS,
   LOG_CHANNEL,
-  UPDATES_CHANNEL,
+  #UPDATES_CHANNEL,
   DATABASE_URL,
   SESSION_NAME
 )
@@ -54,39 +54,6 @@ async def incoming_start_message_f(bot, update):
     """/start command"""
     if not await db.is_user_exist(update.chat.id):
         await db.add_user(update.chat.id)
-    update_channel = UPDATES_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await bot.send_message(
-                   chat_id=update.chat.id,
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                   parse_mode="markdown",
-                   disable_web_page_preview=True
-               )
-               return
-        except UserNotParticipant:
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text="**Please Join My Updates Channel to use this Bot!**",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
-                        ]
-                    ]
-                ),
-                parse_mode="markdown"
-            )
-            return
-        except Exception:
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                parse_mode="markdown",
-                disable_web_page_preview=True)
-            return
     await bot.send_message(
         chat_id=update.chat.id,
         text=Localisation.START_TEXT,
@@ -107,40 +74,6 @@ async def incoming_compress_message_f(bot, update):
   """/compress command"""
   if not await db.is_user_exist(update.chat.id):
       await db.add_user(update.chat.id)
-  update_channel = UPDATES_CHANNEL
-  if update_channel:
-      try:
-          user = await bot.get_chat_member(update_channel, update.chat.id)
-          if user.status == "kicked":
-             await bot.send_message(
-                 chat_id=update.chat.id,
-                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                 parse_mode="markdown",
-                 disable_web_page_preview=True
-             )
-             return
-      except UserNotParticipant:
-          await bot.send_message(
-              chat_id=update.chat.id,
-              text="**Please Join My Updates Channel to use this Bot!**",
-              reply_markup=InlineKeyboardMarkup(
-                  [
-                      [
-                          InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
-                      ]
-                  ]
-              ),
-              parse_mode="markdown"
-          )
-          return
-      except Exception:
-          await bot.send_message(
-              chat_id=update.chat.id,
-              text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-              parse_mode="markdown",
-              disable_web_page_preview=True
-          )
-          return
   if update.reply_to_message is None:
     try:
       await bot.send_message(
