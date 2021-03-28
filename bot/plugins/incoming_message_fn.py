85594403831a95f38163f743b055755886362886
mikiadarsh25 <57398892+mikiadarsh25@users.noreply.github.com>
2021-03-28 17:23:50 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -60,7 +60,7 @@ async def incoming_start_message_f(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
+                    InlineKeyboardButton('0wner', url='@mikiadarsh25')
                 ],
                 [
                     InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
